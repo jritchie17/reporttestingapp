74182d053070b96dfe1069781a330e4020c18119
jritchie17 <99092248+jritchie17@users.noreply.github.com>
2025-07-07 13:30:44 -0500

    docs: note consolidation of union blocks

--- src/analyzer/arreserve.sql
+++ src/analyzer/arreserve.sql
@@ -1,3 +1,6 @@
+-- TODO: Consolidate repeated 'Facility' and 'Anesthesia' UNION blocks using
+-- CASE expressions to reduce duplication. See the review discussion in
+-- GitHub issue #3 for background.
 SELECT DISTINCT
     'Facility: ' +
 	Case WHEN Element3 IN ('Bad debt expense - facility','Contractual adjustments - facility') THEN Element3
